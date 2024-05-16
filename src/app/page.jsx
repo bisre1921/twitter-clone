7327d3bebf7b2ec22adcae82a33d2b9900d0609d
bisre1921 <bisrattewodros3@gmail.com>
2024-05-16 11:13:16 +0300

    create feed component and its header

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import SideBar from "@/component/SideBar";
 import "./globals.css";
+import Feed from "@/component/Feed";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen max-w-7xl mx-auto">
       <SideBar />
+      <Feed />
     </div>
   );
 }
