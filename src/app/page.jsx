f65bbd3e5444c6fd1008f09ca1bd1b400c888a28
bisre1921 <bisrattewodros3@gmail.com>
2024-05-16 15:41:02 +0300

    create the widgets components and the search bar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,14 @@
 import SideBar from "@/component/SideBar";
 import "./globals.css";
 import Feed from "@/component/Feed";
+import Widgets from "@/component/Widgets";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen max-w-7xl mx-auto">
       <SideBar />
       <Feed />
+      <Widgets />
     </div>
   );
 }
