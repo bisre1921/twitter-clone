6395a68f96ae80f2d35d73074ff79e4f29150008
bisre1921 <bisrattewodros3@gmail.com>
2024-05-17 10:41:07 +0300

    create a random users section of the widgets component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,12 +5,13 @@ import Widgets from "@/component/Widgets";
  
 export default async function Home() {
   const newsResults = await fetch("https://saurav.tech/NewsAPI/everything/cnn.json").then((res) => res.json());
+  const randomUsersResult = await fetch("https://randomuser.me/api/?results=30&inc=name,login,picture").then((res) => res.json());
 
   return (
     <div className="flex min-h-screen mx-auto">
       <SideBar />
       <Feed />
-      <Widgets newsResults={newsResults.articles} />
+      <Widgets newsResults={newsResults.articles} randomUsersResult={randomUsersResult.results} />
     </div>
   );
 }
@@ -19,12 +20,3 @@ export default async function Home() {
 
 // https://saurav.tech/NewsAPI/top-headlines/category/business/us.json
 
-// export async function getServerSideProps() {
-//   const newsResults = await fetch("https://saurav.tech/NewsAPI/everything/cnn.json").then((res) => res.json());
-//   return{
-//     props: {
-//       newsResults ,
-//     } ,
-//   }
- 
-// }
