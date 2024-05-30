10823a6aed8654041a8855b6e892ea5789d34933
bisre1921 <bisrattewodros3@gmail.com>
2024-05-30 12:35:33 +0300

    fix deployment errors

--- src/component/Widgets.jsx
+++ src/component/Widgets.jsx
@@ -24,7 +24,7 @@ const Widgets = ({newsResults , randomUsersResult}) => {
 
         <div className='text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]'>
           <h4 className='font-bold text-xl px-4 '>
-            What's happening
+            Whats happening
           </h4>
           <AnimatePresence>
             {newsResults.slice(0 , articleNumber).map((article) => (
